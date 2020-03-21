b66c695ce5224ff68566546ac2205c5d0f473798
knyk <kamfalg@gmail.com>
2020-03-21 14:26:02 +0100

    Created Sylius plugin skeleton and registered Payum gateway factory

---
+++ src/Factory/DotpayGatewayFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Knyk\Payum\Dotpay\Factory;
+
+use Knyk\Payum\Dotpay\Api\DotpayApi;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayFactory;
+
+final class DotpayGatewayFactory extends GatewayFactory
+{
+    protected function populateConfig(ArrayObject $config): void
+    {
+        $config->defaults(
+            [
+                'payum.factory_name' => 'dotpay',
+                'payum.factory_title' => 'Dotpay',
+            ]
+        );
+
+        $config['payum.api'] = fn(ArrayObject $config) => new DotpayApi();
+    }
+}
